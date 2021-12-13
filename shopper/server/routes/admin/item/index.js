7342a82c47792591ed3d2e56d579d56acb863794
Hussam <halshudukhi@channels.com.sa>
2021-12-13 15:58:31 +0300

    Mongoose CRUD

--- shopper/server/routes/admin/item/index.js
+++ shopper/server/routes/admin/item/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 
-const itemService = require("../../../services/ItemService");
+const ItemService = require("../../../services/ItemService");
 
 module.exports = () => {
   const router = express.Router();
